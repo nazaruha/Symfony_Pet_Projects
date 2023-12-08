b99622e4d98e0f739271f3984b3958043fc2c3db
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2023-12-08 13:25:17 +0100

    create movies controller

---
+++ 2_movies/src/Controller/MoviesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    // defaults: ["name" => null] -> default value of the parameter "name" is null. I don't like this way, but I should know it
+    // HEAD == GET, but is called when you pass a huge data
+    #[Route('/movies/{name}', name: 'movies', defaults: ['name' => null], methods: ['GET', 'HEAD'])]
+    public function index($name) : Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MoviesController.php',
+            'name' => "Movie name: $name"
+        ]);
+    }
+}
